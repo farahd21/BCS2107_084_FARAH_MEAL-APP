6de1a4b661e59167ec1c84a67b1d46e0498f267d
farah <farahdarwisyah031221@gmail.com>
2022-11-07 14:44:46 +0800

    First Commit

---
+++ src/main.js
@@ -0,0 +1,97 @@
+var searchBtn = document.getElementById('search-btn');
+var mealList = document.getElementById('meal');
+var mealDetailsContent = document.querySelector('.meal-details-content');
+var recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+
+
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+
+
+// get meal list that matches with the ingredients
+function getMealList(){
+    let searchMeal = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <h4>From : ${meal.strArea}</h4>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } 
+        else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
+}
+
+
+// get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipe(data.meals));
+    }
+}
+
+
+function mealRecipe(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-instruct">
+            <h3>Ingredients:</h3>
+            <p>${meal.strIngredient1}</p>
+            <p>${meal.strIngredient2}</p>
+            <p>${meal.strIngredient3}</p>
+            <p>${meal.strIngredient4}</p>
+            <p>${meal.strIngredient5}</p>
+            <p>${meal.strIngredient6}</p>
+            <p>${meal.strIngredient7}</p>
+            <p>${meal.strIngredient8}</p>
+            <p>${meal.strIngredient9}</p>
+            <p>${meal.strIngredient10}</p>
+        </div>
+        <div class = "meal-name">
+            
+            <a href = "CrudForm.html" class = "go-to-crud-btn">Shopping List</a>
+        </div>
+        
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
